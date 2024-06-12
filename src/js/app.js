8cb353aabf1351fdf89165a6cce5497d00a042a4
Jordi Guiusseppe Martinez Valencia <166647357+JordiBeppe@users.noreply.github.com>
2024-06-12 01:20:12 +0000

    src/js/app.js modified

--- src/js/app.js
+++ src/js/app.js
@@ -33,14 +33,28 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${variables.name == null ? "Nombre" : variables.name} ${
+    variables.lastName == null ? "Apellido" : variables.lastName
+  }</h1>
+          <h2>${variables.role == null ? "Role" : variables.role}</h2>
+          <h3>${variables.country == null ? "city" : variables.country} ${
+    variables.city == null ? "CONTRY" : variables.city
+  }</h3>
+          <ul class= "${variables.socialMediaPosition}"
+        
+                      >
+            <li><a href="${
+              variables.twitter
+            }"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="${
+              variables.github
+            }"><i class="fab fa-github"></i></a></li>
+            <li><a href="${
+              variables.linkedin
+            }"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="${
+              variables.instagram
+            }"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
@@ -54,9 +68,10 @@ window.onload = function() {
     // if includeCover is true the algorithm should show the cover image
     includeCover: true,
     // this is the image's url that will be used as a background for the profile cover
-    background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
+    background: "https://picsum.photos/600/700",
     // this is the url for the profile avatar
-    avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
+    avatarURL:
+      "https://www.svgrepo.com/show/384674/account-avatar-profile-user-11.svg",
     // social media bar position (left or right)
     socialMediaPosition: "position-left",
     // social media usernames
